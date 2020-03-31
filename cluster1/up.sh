8a8489694b495d30ad50e3ed6f901f893d3ecbff
Moritz Graf <moritz.graf@continental-corporation.com>
2020-03-31 09:18:36 +0200

    Using vagrant to bootstrap k8s examples

--- cluster1/up.sh
+++ cluster1/up.sh
@@ -6,7 +6,7 @@ sleep 60
 
 echo '######################## INITIALISING K8S RESOURCES ########################'
 chmod 400 certs/id_rsa
-ssh-keygen -R 192.168.101.101; ssh root@192.168.101.101 -i certs/id_rsa -oStrictHostKeyChecking=no -- kubectl apply -f /vagrant/k8s/init.yaml
+vagrant ssh cluster1-master1 -c "sudo su - root -c 'kubectl apply -f /vagrant/k8s/init.yaml'"
 sleep 20
 
 echo '######################## ALL DONE ########################'
