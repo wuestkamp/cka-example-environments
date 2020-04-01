6e29609a50472a2ffa7285ce5a8f0f95aa9ab32e
Moritz Graf <moritz.graf@continental-corporation.com>
2020-04-01 15:07:40 +0200

    Moving kubeconfig to user vagrant

--- cluster1/up.sh
+++ cluster1/up.sh
@@ -7,6 +7,7 @@ sleep 60
 echo '######################## INITIALISING K8S RESOURCES ########################'
 chmod 400 certs/id_rsa
 vagrant ssh cluster1-master1 -c "sudo su - root -c 'kubectl apply -f /vagrant/k8s/init.yaml'"
+vagrant ssh cluster1-master1 -c "mkdir -p .kube ; sudo cp /root/.kube/config ./.kube/config ; sudo chown vagrant:vagrant .kube/config"
 sleep 20
 
 echo '######################## ALL DONE ########################'
